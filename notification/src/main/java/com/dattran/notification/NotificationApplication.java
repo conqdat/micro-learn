3cc39ba55a3ee54edb0573ce47fc5b0f26de907c
DatTran <trancongdat.datct@gmail.com>
2023-10-03 23:02:22 +0700

    add code for implements functions of RabbitMQ

--- notification/src/main/java/com/dattran/notification/NotificationApplication.java
+++ notification/src/main/java/com/dattran/notification/NotificationApplication.java
@@ -1,13 +1,36 @@
 package com.dattran.notification;
 
+import com.dattran.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-@EnableEurekaClient // It acts as a eureka client
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.dattran.notification",
+                "com.dattran.amqp",
+        }
+)
+//@EnableEurekaClient // It acts as a eureka client
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer rabbitMQMessageProducer,
+//            NotificationConfig notificationConfig
+//    ) {
+//        return args -> {
+//            rabbitMQMessageProducer.publish(
+//                    new Person("Dat Tran", 25),
+//                    notificationConfig.getInternalExChange(),
+//                    notificationConfig.getInternalNotificationRoutingKey()
+//            );
+//        };
+//    }
+//
+//    record Person(String name, int age) {}
 }
