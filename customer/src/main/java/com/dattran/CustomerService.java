ff819b79737c97501e0683bd925a423ca8e04148
dattran <trancongdat.datct@gmail.com>
2024-05-30 23:55:20 +0700

    add rest template (1:21:50)

--- customer/src/main/java/com/dattran/CustomerService.java
+++ customer/src/main/java/com/dattran/CustomerService.java
@@ -2,17 +2,30 @@ package com.dattran;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
+
+    public record FraudCheckResponse(Boolean isFraudster) {
+    }
+
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRequest.firstName())
                 .lastName(customerRequest.lastName())
                 .email(customerRequest.email())
                 .build();
-        Customer savedCustomer = customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8080/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
+        if(fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("Fraudster check failed");
+        }
     }
 }
