9668653c3541605c8071a6bd1bb1e1b0c90eecd9
dattran <trancongdat.datct@gmail.com>
2024-06-02 23:48:54 +0700

    add noti service (2:10:29)

--- customer/src/main/java/com/dattran/CustomerService.java
+++ customer/src/main/java/com/dattran/CustomerService.java
@@ -2,6 +2,8 @@ package com.dattran;
 
 import com.dattran.clients.fraud.FraudCheckResponse;
 import com.dattran.clients.fraud.FraudClient;
+import com.dattran.clients.notification.NotificationClient;
+import com.dattran.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
 
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
@@ -24,5 +27,15 @@ public class CustomerService {
         if(fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("Fraudster check failed");
         }
+
+        // todo: make it async. i.e add to queue
+        notificationClient.sendNotification(
+                new NotificationRequest(
+                        customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi %s, welcome to Dat Tran...",
+                                customer.getFirstName())
+                )
+        );
     }
 }
