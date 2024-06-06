bd5bd3da345efedd36030d14530ee36e647ad4d6
dattran <trancongdat.datct@gmail.com>
2024-06-07 00:14:11 +0700

    add code (3:5:53)

--- amqp/src/main/java/com/dattran/RabbitMQConfig.java
+++ amqp/src/main/java/com/dattran/RabbitMQConfig.java
@@ -4,14 +4,14 @@ package com.dattran;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
-@Configuration
+@ComponentScan
 @AllArgsConstructor
 public class RabbitMQConfig {
 
--- notification/src/main/java/com/dattran/NotificationApplication.java
+++ notification/src/main/java/com/dattran/NotificationApplication.java
@@ -1,11 +1,30 @@
 package com.dattran;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.dattran.notification",
+                "com.dattran.amqp",
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer,
+            NotificationConfig notificationConfig
+
+    ) {
+        return args -> {
+            producer.publish("foo", notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
     }
 }
